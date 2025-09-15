405763a3ca723d5278f0f0695ead14ef182d9494
Shubh Kamra <114826841+gaashvik@users.noreply.github.com>
2025-09-15 08:00:01 +0530

    Update Hero component title and description

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,11 +6,11 @@ const Hero = () => (
   <div className="hero my-5 text-center" data-testid="hero">
     <Logo testId="hero-logo" />
     <h1 className="mb-4" data-testid="hero-title">
-      Next.js Sample Project
+      N Device Limit
     </h1>
 
     <p className="lead" data-testid="hero-lead">
-      This is a sample application that demonstrates an authentication flow for a Regular Web App, using{' '}
+      Implemented using FastAPI, Auth0 & Next.js{' '}
       <a href="https://nextjs.org">Next.js</a>
     </p>
   </div>
